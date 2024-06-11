8b66c4212ac2c1679b9f99ee6ba71fea4a47a008
Matthew Campbell <m.campbell92@gmail.com>
2024-06-11 10:47:56 +0200

    set up data route, template; change styles

--- routes/movies.js
+++ routes/movies.js
@@ -5,31 +5,75 @@ const logger = require("../config/logger");
 router.get("/search", async (req, res, next) => {
   const searchTerm = req.query.q; // https://www.shecodes.io/athena/72173-what-does-req-query-do-in-express-js
   try {
-    const rows = await req.db("basics")
+    const rows = await req
+      .db("basics")
       .select("primaryTitle", "startYear", "genres")
       .where("primaryTitle", "like", `%${searchTerm}%`); // https://www.w3schools.com/sql/sql_wildcards.asp
-    res.render("movies/search", {
-      title: "PopcornBase",
-      movies: rows,
-      searchTerm: searchTerm,
-    });
+
+    if (!searchTerm) {
+      res.render("movies/search", {
+        title: "PopcornBase",
+        error: "You must provide a title!",
+      });
+    } else if (rows.length === 0) {
+      res.render("movies/search", {
+        title: "PopcornBase",
+        error: "Movie not found. Please try a different search term.",
+      });
+    } else {
+      res.render("movies/search", {
+        title: "PopcornBase",
+        movies: rows,
+        searchTerm: searchTerm,
+        error: " ",
+      });
+    }
   } catch (err) {
     logger.error(err);
     res.render("movies/search", {
-      error: "Error in query",
+      error: "Error in database query",
     });
   }
 });
 
-router.get("/data", (req, res) => {
+// handlebars if statements: https://www.sitepoint.com/a-beginners-guide-to-handlebars/
+
+router.get("/data", async (req, res, next) => {
+  const searchTerm = req.query.q;
+  try {
+    const rows = await req
+      .db("basics")
+      .select("primaryTitle", "startYear", "genres")
+      .where("tconst", "like", `%${searchTerm}%`);
+
+    if (!searchTerm) {
+      res.render("movies/data", {
+        title: "PopcornBase",
+        error: "You must provide an imdb ID number!",
+      });
+    } else if (rows.length === 0) {
+      res.render("movies/data", {
+        title: "PopcornBase",
+        error: "Movie not found. Please try a different search term.",
+      });
+    } else {
+      res.render("movies/data", {
+        title: "PopcornBase",
+        movies: rows,
+        searchTerm: searchTerm,
+        error: " ",
+      });
+    }
+  } catch (err) {
+    logger.error(err);
     res.render("movies/data", {
-        title: "PopcornBase"
+      title: "PopcornBase",
+      error: "Error in database query.",
     });
-    logger.info("landed @ imdb id search page")
+  }
 });
 
 module.exports = router;
-
 
 // res.render("movies/search", {
 //     title: "PopcornBase"
