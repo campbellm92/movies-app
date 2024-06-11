dca4c50f129d153e62caeb50b41267a22e85aff1
Matthew Campbell <m.campbell92@gmail.com>
2024-06-11 12:49:57 +0200

    set up posters route & fetch call to omdb

--- routes/posters.js
+++ routes/posters.js
@@ -1,14 +1,50 @@
 const express = require("express");
 const router = express.Router();
 const logger = require("../config/logger");
+require("dotenv").config({ path: "../.env" });
 
-router.get("/", (req, res) => {
-    res.render("posters/posters", {
-        title: "PopcornBase"
+const OMDB_BASE = "https://www.omdbapi.com/?apikey="
+const OMDB_API_KEY = process.env.OMDB_KEY;
+
+// tt0027977
+
+router.get("/", async (req, res, next) => {
+  const searchTerm = req.query.q;
+  const url = OMDB_BASE + OMDB_API_KEY + "&i=" + searchTerm;
+
+  if (searchTerm === undefined) {
+    return res.render("posters/posters", {
+      title: "PopcornBase",
     });
-    logger.info("landed @ poster search page");
+  }
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    if (!data || data.Response === "False") {
+      return res.render("posters/posters", {
+        title: "PopcornBase",
+        error: "No movies found",
+      });
+    } else {
+      res.render("posters/posters", {
+        title: "PopcornBase",
+        poster: data.Poster,
+        searchTerm: searchTerm,
+        error: " ",
+      });
+    }
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 
+// const json = await res.json();
+// console.log(json);
+// } catch (err) {
+// console.log(err);
+// }
 
 module.exports = router;
