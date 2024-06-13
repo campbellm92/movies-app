218229e11726105369116566c4cc6d8c3a2c8dcc
Matthew Campbell <m.campbell92@gmail.com>
2024-06-13 12:48:03 +0200

    initialise poster add functionality/route

--- routes/posters.js
+++ routes/posters.js
@@ -1,20 +1,23 @@
 const express = require("express");
 const router = express.Router();
 const logger = require("../config/logger");
+const upload = require("../middleware/multerUpload");
 require("dotenv").config({ path: "../.env" });
 
 const OMDB_BASE = "https://www.omdbapi.com/?apikey="
 const OMDB_API_KEY = process.env.OMDB_KEY;
 
-// tt0027977
+const siteTitle = "PopcornBase";
 
+// tt0027977
+// find posters route
 router.get("/", async (req, res, next) => {
   const searchTerm = req.query.q;
   const url = OMDB_BASE + OMDB_API_KEY + "&i=" + searchTerm;
 
   if (searchTerm === undefined) {
     return res.render("posters/posters", {
-      title: "PopcornBase",
+      title: siteTitle,
     });
   }
 
@@ -24,12 +27,12 @@ router.get("/", async (req, res, next) => {
 
     if (!data || data.Response === "False") {
       return res.render("posters/posters", {
-        title: "PopcornBase",
+        title: siteTitle,
         error: "No movies found",
       });
     } else {
       res.render("posters/posters", {
-        title: "PopcornBase",
+        title: siteTitle,
         poster: data.Poster,
         searchTerm: searchTerm,
         error: " ",
@@ -39,6 +42,29 @@ router.get("/", async (req, res, next) => {
     console.log(err);
   }
 });
+
+//upload posters route
+
+router.get("/add", (req, res, next) => {
+  res.render("posters/add", {
+    title: siteTitle,
+  })
+})
+
+router.post("/add", upload.single("image"), (req, res) => {
+  if(!req.file) {
+    res.render("posters/add", {
+      title: siteTitle,
+      error: "Upload failed"
+    });
+  } else {
+    res.render("posters/add", {
+      title: siteTitle,
+      message: "Image uploaded successfully."
+    });
+  };
+});
+
 
 module.exports = router;
 
