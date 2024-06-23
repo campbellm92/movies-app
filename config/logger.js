309521360d61c024bbfb0b0cb9f8104c166b00d4
Matthew Campbell <m.campbell92@gmail.com>
2024-06-23 17:29:39 +0200

    remove comments

--- config/logger.js
+++ config/logger.js
@@ -21,7 +21,3 @@ const logger = winston.createLogger({
 });
 
 module.exports = logger;
-
-// sources:
-// https://www.npmjs.com/package/winston?activeTab=readme
-// https://betterstack.com/community/guides/logging/how-to-install-setup-and-use-winston-and-morgan-to-log-node-js-applications/
